24c045a26c9935e83f4acf88efa2807594bae54d
Luca Sartori <lucasartori94@gmail.com>
2020-08-31 20:58:16 -0300

    adding CORS

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
-  const port = process.env.PORT || 3000;
+  if (configService.get('NODE_ENV') === 'development') {
+    app.enableCors();
+  } else {
+    const origin = configService.get('HOSTNAME');
+    app.enableCors({
+      origin: origin
+    });
+    logger.log(`Accepting requests from origin ${origin}`)
+  }
+
+  const port = configService.get('PORT', 3000);
   await app.listen(port);
 
   logger.log(`Application listening on port ${port}`)
